295b225f04d12abf85dd8b2e4a1cf1fa21f6cae2
mahammadbakr <mahammadbakr77@gmail.com>
2022-04-12 11:58:29 +0300

    start implement UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="gateway-test-app">
+      <header className="app-header">
+        <h1 className="app-title">Gateway App Test</h1>
+      </header>
+      <div className="container pt-5">
+      </div>
+    </div>
+  );
+}
+
+export default App;
